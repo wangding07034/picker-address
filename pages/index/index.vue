6355273f631b6e9dfe308770302cf5dc5dddccb9
wangding <857299269@qq.com>
2020-02-21 15:42:28 +0800

    'init'

---
+++ pages/index/index.vue
@@ -0,0 +1,29 @@
+<template>
+	<view class="content">
+		<pickerAddress @change="change">{{txt}}</pickerAddress>
+	</view>
+</template>
+
+<script>
+	import pickerAddress from '../../components/wangding-pickerAddress/wangding-pickerAddress.vue'
+	export default {
+		components:{
+			pickerAddress
+		},
+		data() {
+			return {
+				txt: '选择地址',
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			change(data) {
+				this.txt = data.data.join('')
+				console.log(data.data.join(''))
+			}
+		}
+	}
+</script>
